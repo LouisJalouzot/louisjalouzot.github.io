e7e62bc8e2f495b2f6f65c89cbc688820bb5298d
Louis Jalouzot <louis.jalouzot@ens-lyon.fr>
2025-03-01 12:38:58 +0100

    fix(config): update ignore patterns and modify footer component; add new OG icon

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "louisjalouzot.github.io",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: true,
     theme: {
